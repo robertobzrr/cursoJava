42dfcb828227770daebb18f67c4439649b6b40f5
robertobzrr <robertobezerrafh@outlook.com>
2025-09-08 16:27:59 -0300

    patch: finalizando exercicios lista if/else

--- src/exercicios/ex_sec06/ex07.java
+++ src/exercicios/ex_sec06/ex07.java
@@ -6,7 +6,35 @@ public class ex07 {
 
         Scanner sc = new Scanner(System.in);
 
+        double x, y;
+
+        System.out.println("Vamos mexer com plano cartesiano");
+        System.out.println("Escolha os valores de X e Y ");
+        System.out.print("X = ");
+        x = sc.nextDouble();
+        sc.nextLine();
+        System.out.print("Y = ");
+        y = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.printf("VOcê escolheu a coordenada: (%.1f, %.1f)%n%n", x,y);
 
 
+        System.out.println("Agora vamos considerar o plano cartesiado");
+
+        if (x > 0 && y > 0){
+            System.out.println("Você está no quadrante Q1");
+        }else if (x>0 && y<0){
+            System.out.println("Você está no quadrante Q4");
+        }else if (x<0 && y>0){
+            System.out.println("Você está no quadrante Q2");
+        }else if (x<0 && y<0){
+            System.out.println("Você está no quadrante Q3");
+        }else {
+            System.out.println("você está na Origem (0,0)");
+        }
+
+
+        sc.close();
     }
 }
--- src/exercicios/ex_sec06/ex08.java
+++ src/exercicios/ex_sec06/ex08.java
@@ -1,4 +1,35 @@
 package exercicios.ex_sec06;
+import java.util.Scanner;
 
 public class ex08 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double salario, imposto;
+
+        System.out.println("====== Calculadora de Imposto de Renda ======");
+        System.out.print("Digite seu salário: ");
+        salario = sc.nextDouble();
+        sc.nextLine();
+
+        if (salario <= 2000){
+            System.out.println("ISENTO");
+        }else if (salario > 2000 && salario <= 3000){
+            System.out.println("Você pagará 8% de impostos");
+            imposto = salario * 0.08;
+            System.out.printf("Imposto = R$%.2f", imposto);
+        }else if (salario > 3000 && salario <= 4500){
+            System.out.println("Você pagará 18% de impostos");
+            imposto = salario *0.15;
+            System.out.printf("Imposto = R$%.2f", imposto);
+        }else if (salario > 4500){
+            System.out.println("Você pagará 28% de impostos");
+            imposto = salario *0.28;
+            System.out.printf("Imposto = R$%.2f", imposto);
+        }
+
+
+        sc.close();
+    }
 }
