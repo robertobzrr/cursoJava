6391c6761d07fbe49d67684916ad6e5de2fe7cb8
robertobzrr <robertobezerrafh@outlook.com>
2025-09-11 10:05:52 -0300

    patch: revisando POO

---
+++ src/Section08/base_poo.java
@@ -0,0 +1,42 @@
+package Section08;
+import java.util.Scanner;
+
+public class base_poo {
+    public static void main(String[] args) {
+
+     Scanner input = new Scanner(System.in);
+     Quadrado x = new Quadrado();
+
+     System.out.println("Calcular a área de um quadrado");
+     System.out.println("Digite os valores das suas bases.");
+
+     System.out.print("Base A: ");
+     x.baseA = input.nextInt();
+     input.nextLine();
+
+     System.out.print("Base B: ");
+     x.baseB = input.nextInt();
+     input.nextLine();
+
+
+        System.out.println("Área = "+ x.area());
+
+
+    }
+
+
+    public static class Quadrado {
+
+        public double baseA;
+        public double baseB;
+
+        public double area(){
+            return baseA * baseB;
+
+        }
+
+
+    }
+
+
+}
