427b994b160a74d9a0a989e8dfefb93e95e74948
robertobzrr <robertobezerrafh@outlook.com>
2025-09-15 19:48:56 -0300

    identando

--- src/exercicios/ex_sec09/ex01/application/Main.java
+++ src/exercicios/ex_sec09/ex01/application/Main.java
@@ -15,11 +15,8 @@ public class Main {
         int numeroConta = input.nextInt();
         input.nextLine();
 
-
         System.out.print("Nome do Cliente: ");
         String donoConta = input.nextLine();
-
-
 
         System.out.println("\nDejesa fazer um deposito incial?\n[1] - Sim\n[0] - Não");
         int escolha = input.nextInt();
@@ -35,24 +32,17 @@ public class Main {
                 accountManager = new accountManager(numeroConta, donoConta);
             }
 
-
         System.out.printf("%n=========================%nNº da conta: %d%nDono: %s%nSaldo: %.2f %n=========================%n%n", accountManager.getNumeroConta(), accountManager.getDonoConta(), accountManager.getDepositoInicial());
-
-
         System.out.print("Novo Depósito: ");
         double deposito = input.nextDouble();
         accountManager.deposito(deposito);
+
         System.out.printf("%n=========================%nNº da conta: %d%nDono: %s%nSaldo: %.2f %n=========================%n%n", accountManager.getNumeroConta(), accountManager.getDonoConta(), accountManager.getDepositoInicial());
-
-
         System.out.print("Saque: ");
         double saque = input.nextDouble();
         accountManager.saque(saque);
+
         System.out.printf("%n=========================%nNº da conta: %d%nDono: %s%nSaldo: %.2f %n=========================%n%n",accountManager.getNumeroConta(), accountManager.getDonoConta(), accountManager.getDepositoInicial());
-
-
-
-
         input.close();
     }
 }
--- src/exercicios/ex_sec09/ex01/entities/accountManager.java
+++ src/exercicios/ex_sec09/ex01/entities/accountManager.java
@@ -7,7 +7,6 @@ public class accountManager {
     private String donoConta;
     private double saldo;
 
-
     public accountManager(int numeroConta, String donoConta, double depositoInicial) {
         this.numeroConta = numeroConta;
         this.donoConta = donoConta;
@@ -18,7 +17,6 @@ public class accountManager {
         this.numeroConta = numeroConta;
         this.donoConta = donoConta;
     }
-
 
     public int getNumeroConta() {
         return numeroConta;
@@ -33,9 +31,6 @@ public class accountManager {
         return saldo;
     }
 
-
-
-
     public void deposito(double deposito){
         this.saldo += deposito;
     }
@@ -43,10 +38,6 @@ public class accountManager {
     public void saque(double saque){
         this.saldo -= saque+5;
     }
-
-
-    //dúvida para levar na aula: como o metodo deposito no construtor pode estar enviando a variavel depositoInicial
-    // para o metodo deposito se no metodo ele ta recebendo double deposito, como é possivel e como o java entende o envio de deposito(depositoIncial) para deposito(double deposito)
 
 
 }
